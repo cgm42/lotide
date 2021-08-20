9411555aadf2de4eff35aeaac8f7df4a41ee7e98
Carrie <carr1eg@hotmail.com>
2021-08-20 14:36:15 -0700

    implemented recursion to compare multiD arrays

--- eqArrays.js
+++ eqArrays.js
@@ -8,13 +8,21 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = (arr1, arr2) => {
+  if(!Array.isArray(arr1) || !Array.isArray(arr2)) { //compare primitives
+    return arr1 === arr2;
+  }
   if(arr1.length !== arr2.length) return false;
   for (let i = 0; i < arr1.length; i++ ) {
-    if(arr1[i] !== arr2[i]) return false;
+      if(!eqArrays(arr1[i], arr2[i])) return false;
   }
   return true;
 }
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true) // => true
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false) // => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false) // => false
+assertEqual(eqArrays([[1, 2, [[3], 4]], 5, []], [[1, 2, [[3], 4]], 5, []]), true)
+assertEqual(eqArrays([false,[[[[[[[[[[[[1]]]]]]]]]]]],9], [false,[[[[[[[[[[[[1]]]]]]]]]]]],9]), true) // => true
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays(["1",2], [1, 2]), true); // => should FAIL
-assertEqual(eqArrays([], []), true); // => should PASS
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+// assertEqual(eqArrays(["1",2], [1, 2]), true); // => should FAIL
+// assertEqual(eqArrays([], []), true); // => should PASS
