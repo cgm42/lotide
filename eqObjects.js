2046deb52fa6b6539001230d2232f823c6b6b8c9
Carrie <carr1eg@hotmail.com>
2021-08-20 14:35:03 -0700

    implemented recursion to compare multi-D objects

--- eqObjects.js
+++ eqObjects.js
@@ -15,41 +15,37 @@ const eqArrays = (arr1, arr2) => {
   return true;
 }
 
-// Returns true if both objects have identical keys with identical values.
-// Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
+  if(typeof object1 !== 'object') {
+    if(!Array.isArray(object1)) {
+      return object1 === object2;
+    }
+  }
   if(Object.keys(object1).length !== Object.keys(object2).length) return false;
   for(let key of Object.keys(object1)) {
     if(Array.isArray(object1[key])) {
       if(!eqArrays(object1[key], object2[key])) return false;
-    } else if (object1[key] !== object2[key]) {
-      return false;
-    }
+    } else {//recurse if object or primitive
+      if(!eqObjects(object1[key], object2[key])) return false;
+    } 
   }
   return true;
 };
+console.log(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })) // => true
+console.log(eqObjects({ a: { z: {zz: 1} }, b: 2 }, { a: { z: {zz: 1} }, b: 2 })) // => true
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })) // => false
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 })) // => false
 
+// const ab = { a: "1", b: "2" };
+// const ba = { b: "2", a: "1" };
+// console.log(eqObjects(ab, ba)); // => true
 
+// const abc = { a: "1", b: "2", c: "3" };
+// console.log(eqObjects(ab, abc)); // => false
 
+// const cd = { c: "1", d: ["2", 3] };
+// const dc = { d: ["2", 3], c: "1" };
+// console.log(eqObjects(cd, dc)); // => true
 
-
-
-
-
-
-
-
-
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-console.log(eqObjects(ab, ba)); // => true
-
-const abc = { a: "1", b: "2", c: "3" };
-console.log(eqObjects(ab, abc)); // => false
-
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-console.log(eqObjects(cd, dc)); // => true
-
-const cd2 = { c: "1", d: ["2", 3, 4] };
-console.log(eqObjects(cd, cd2)); // => false
+// const cd2 = { c: "1", d: ["2", 3, 4] };
+// console.log(eqObjects(cd, cd2)); // => false
